1e3149f0a0be484d37f7fa8173f1b707d2f902d3
Scott <scottmjanssen@gmail.com>
2022-08-25 21:16:55 -0400

    initial commit

---
+++ src/test/java/com/cs3300/LocationSearch/LocationSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs3300.LocationSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
