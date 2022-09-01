6443754e1af55c59ba0928b7c263d67d079f5e47
Scott <scottmjanssen@gmail.com>
2022-09-01 14:01:39 -0400

    Adding textfields for map entries and search button

--- react/location-search/src/index.js
+++ react/location-search/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import LandingPage from "./LandingPage";
+import UserPage from "./UserPage";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LandingPage/>
+    <UserPage/>
   </React.StrictMode>
 );
 
